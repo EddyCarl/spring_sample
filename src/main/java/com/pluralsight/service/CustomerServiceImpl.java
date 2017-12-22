d3332f51d1c44103803f3ed7509f85e61dc182b8
Carl.Edmunds <carledmunds90@googlemail.com>
2017-12-22 12:47:14 +0000

    Starting to use auto-wiring
    Uses auto-wiring by type. This will look for a setter that uses a type that matches the CustomerRepository type.

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -9,6 +9,11 @@ public class CustomerServiceImpl implements CustomerService
 {
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl()
+    {
+
+    }
+
     public CustomerServiceImpl(CustomerRepository customerRepository)
     {
         this.customerRepository = customerRepository;
