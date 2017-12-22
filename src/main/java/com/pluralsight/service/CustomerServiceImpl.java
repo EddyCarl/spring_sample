ccb202bae59c97140da854dd8284c88678435bfb
Carl.Edmunds <carledmunds90@googlemail.com>
2017-12-22 12:37:49 +0000

    Changing the base implementation to make use of constructor injection as opposed to setter injection.
    Constructor injection works by taking any arguments as indicies - As seen in the XML file (One index which passes a customerRepository instance as required).

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -7,8 +7,12 @@ import com.pluralsight.repository.CustomerRepository;
 
 public class CustomerServiceImpl implements CustomerService
 {
-
     private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository)
+    {
+        this.customerRepository = customerRepository;
+    }
 
     public void setCustomerRepository(CustomerRepository customerRepository)
     {
