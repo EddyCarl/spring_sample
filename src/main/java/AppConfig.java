bd2e843fbfcb0aeda33904ef818f066642a9cb37
Carl.Edmunds <carledmunds90@googlemail.com>
2018-01-02 11:29:52 +0000

    Removing all defined beans from AppConfig - Completely autowired
    - Removing the beans defined in AppConfig - They are no longer required as we are using Spring to autowire the entire application
    - CustomerServiceImpl is now annotated as a Service bean with @Service
    - The bean is called "customerService" - Not really required, but good practice

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,3 @@
-import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,15 +5,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan({"com.pluralsight"})
 public class AppConfig
 {
-    @Bean(name = "customerService")
-    public CustomerService getCustomerService()
-    {
-        CustomerServiceImpl service = new CustomerServiceImpl();
-        return service;
-    }
-
-    // No longer need a bean defined for the customerRepository as the
-    // autowired annotation allows this to occur due to the fact that
-    // the HibernateImpl class is within the "com.pluralsight" package
-    // that we are component scanning on.
+    // No beans defined at all now!
 }
